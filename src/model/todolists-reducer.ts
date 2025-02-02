ab68b89aab57151c80552786bbeb71e912ad1079
Michael Dudzik <mkdudik15@gmail.com>
2025-02-02 19:37:16 +0300

    change method of output data

--- src/model/todolists-reducer.ts
+++ src/model/todolists-reducer.ts
@@ -1,70 +1,47 @@
 import {FilterValues, Todolist} from "../app/App.tsx";
+import {createAction, createReducer, nanoid} from "@reduxjs/toolkit";
+
 
 // Типизация для возможных действий
-type ActionType =
-    CreateTodolistActionType
-    | DeleteTodolistActionType
-    | ChangeTodolistTitleActionType
-    | ChangeTodolistFilterType;
 
-export const todolistsReducer = (todolists: Todolist[]=[], action: ActionType): Todolist[] => {
-    switch (action.type) {
-        case 'delete_todolist': {
-            return todolists.filter(todolist => todolist.id !== action.payload.id);
-        }
-        case 'create_todolist': {
-            const {id, title} = action.payload; // Деструктурируем корректно
-            const newTodolist: Todolist = {id, title, filter: 'all'};
-            return [newTodolist, ...todolists];
-        }
-        case 'change_todolist_title': {
-            const {id, title} = action.payload; // Деструктурируем корректно
-            return todolists.map(todolist =>
-                todolist.id === id ? {...todolist, title} : todolist
-            );
-        }
-        case 'change_todolist_filter' :{
-            const {id, filter} = action.payload;
-            const nextState=todolists.map(todolist => todolist.id === id ? { ...todolist, filter } : todolist)
-            return nextState;
-        }
-        default:
-            return todolists;
-    }
-};
+const initialState: Todolist[] = []
 
-// Action Creator для удаления тудулиста
-export const DeleteTodolistAC = (id: string) => ({
-    type: 'delete_todolist',
-    payload: {id},
-} as const);
+export const deleteTodolistAC = createAction<{ id: string }>('todolists/deleteTodolist')
+export const changeTodolistTitleAC = createAction<{ id: string, title: string }>('todolists/changeTodolistTitle')
+export const createTodolistAC = createAction('todolists/createTodolist', (title: string) => {
+    return {payload: {title, id: nanoid()}}
+})
+export const changeTodolistFilterAC = createAction<{
+    id: string,
+    filter: FilterValues
+}>('todolists/changeTodolistFilter')
 
-// Тип для удаления
-export type DeleteTodolistActionType = ReturnType<typeof DeleteTodolistAC>;
+export const todolistsReducer = createReducer(initialState, builder => {
+    builder
+        .addCase(deleteTodolistAC, (state, action) => {
+            const index = state.findIndex(tdl => tdl.id === action.payload.id)
+            if (index !== -1) {
+                state.splice(index, 1)
+            }
 
-// Action Creator для создания тудулиста
-export const CreateTodolistAC = (title: string, id: string) => ({
-    type: 'create_todolist',
-    payload: {
-        id,
-        title,
-    },
-} as const);
+        })
+        .addCase(changeTodolistTitleAC,(state, action)=>{
+            const index = state.findIndex(tdl => tdl.id === action.payload.id)
+            if (index !== -1) {
+                state[index].title = action.payload.title
+            }
+        })
+        .addCase(createTodolistAC,(state,action)=>{
+            state.unshift({...action.payload,filter:'all'})
+        })
+        .addCase(changeTodolistFilterAC,(state, action)=>{
+            const todolist=state.find(tdl=>tdl.id===action.payload.id)
+            if(todolist){
+                todolist.filter=action.payload.filter
+            }
+        })
+})
 
-// Тип для создания
-export type CreateTodolistActionType = ReturnType<typeof CreateTodolistAC>;
 
-// Action Creator для изменения названия тудулиста
-export const ChangeTodolistTitleAC = (payload: { id: string; title: string }) => ({
-    type: 'change_todolist_title',
-    payload,
-} as const);
 
-// Тип для изменения названия
-export type ChangeTodolistTitleActionType = ReturnType<typeof ChangeTodolistTitleAC>;
 
-export const ChangeTodolistFilterAC = (payload: { id: string, filter: FilterValues }) => ({
-    type: 'change_todolist_filter',
-    payload
-} as const)
-export type ChangeTodolistFilterType = ReturnType<typeof ChangeTodolistFilterAC>
