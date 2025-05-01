4e16804fb2fa193ba4d9fecd5fbecece030fb618
Michael Dudzik <mkdudik15@gmail.com>
2025-05-01 23:57:39 +0300

    updating file

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-const token = '52a8871e-b50e-407c-b7bf-b804846a54d9'
-const apiKey = '5fad9655-7393-4794-9838-c3907ba2a60b'
+const token =import.meta.env.VITE_AUTH_TOKEN
+const apiKey = import.meta.env.VITE_API_KEY
 
 export const instance = axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.1',
+    baseURL: import.meta.env.VITE_BASE_URL,
     headers: {
         Authorization: `Bearer ${token}`,
         'API-KEY': apiKey,
