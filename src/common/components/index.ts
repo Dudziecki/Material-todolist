2916d7d7d3af302f6360ff10509e1742dcdf6533
Michael Dudzik <mkdudik15@gmail.com>
2025-06-17 23:18:49 +0300

    exports

---
+++ src/common/components/index.ts
@@ -0,0 +1,6 @@
+export { CreateItemForm } from "./CreateItemForm/CreateItemForm"
+export { EditableSpan } from "./EditableSpan/EditableSpan"
+export { Header } from "./Header/Header"
+export { NavButton } from "./NavButton/NavButton"
+export { ErrorSnackbar } from "./ErrorSnackbar/ErrorSnackbar.tsx"
+export { PageNotFound } from "./PageNotFound/PageNotFound.tsx"
