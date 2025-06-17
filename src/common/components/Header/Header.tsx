d5f50c314c3c1d9eb3001ee7f3eea143012eb1fb
Michael Dudzik <mkdudik15@gmail.com>
2025-06-17 23:22:36 +0300

    add isLogin logic

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,44 +1,63 @@
+import { changeThemeModeAC, selectIsLoggedIn, selectStatus, selectThemeMode, setIsLoggedInAC } from "@/app/app-slice.ts"
 
-import Toolbar from "@mui/material/Toolbar";
-import {Container} from "@mui/material";
-
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import {MaterialUISwitch} from "@/Switch.ts";
-import {NavButton} from "@/common/components/NavButton/NavButton.ts";
-import AppBar from "@mui/material/AppBar";
-import {changeThemeModeAC, selectThemeMode} from "@/app/app-slice.ts";
-import {getTheme} from "@/common/theme/theme.ts";
-import {useAppSelector} from "@/common/hooks/useAppSelector.ts";
-
-import {useAppDispatch} from "@/common/hooks/useAppDispatch.ts";
-import {containerSx} from "@/common/styles/container.styles.ts";
+import { useAppDispatch, useAppSelector } from "@/common/hooks"
+import { containerSx } from "@/common/styles"
+import { getTheme } from "@/common/theme"
+import { NavButton } from "@/common/components/NavButton/NavButton"
+import MenuIcon from "@mui/icons-material/Menu"
+import AppBar from "@mui/material/AppBar"
+import Container from "@mui/material/Container"
+import IconButton from "@mui/material/IconButton"
+import Switch from "@mui/material/Switch"
+import Toolbar from "@mui/material/Toolbar"
+import { LinearProgress } from "@mui/material"
+import { ResultCode } from "@/common/enums"
+import { useLogoutMutation } from "@/features/auth/api/authApi.ts"
+import { AUTH_TOKEN } from "@/common/constants"
+import { baseApi } from "@/app/baseApi.ts"
 
 export const Header = () => {
-    const themeMode = useAppSelector(selectThemeMode)
-    const dispatch = useAppDispatch()
-    const onSwitchModeChangeHandler=()=>{
-        dispatch(changeThemeModeAC({themeMode: themeMode === 'light' ? 'dark' : 'light'}))
-    }
-    const theme = getTheme(themeMode)
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                <Container sx={containerSx}>
-                    <IconButton color="inherit">
-                        <MenuIcon />
-                    </IconButton>
-                    <div>
-                        <MaterialUISwitch onChange={onSwitchModeChangeHandler}/>
-                        <NavButton color="secondary">Sign in</NavButton>
-                        <NavButton color="inherit">Sign in</NavButton>
-                        <NavButton background={theme.palette.primary.dark} color="inherit">faq</NavButton>
-                    </div>
-                </Container>
+  const themeMode = useAppSelector(selectThemeMode)
+  const status = useAppSelector(selectStatus)
+  const isLoggedIn = useAppSelector(selectIsLoggedIn)
 
+  const [logout] = useLogoutMutation()
+  const dispatch = useAppDispatch()
 
-            </Toolbar>
-        </AppBar>
-    );
-};
+  const theme = getTheme(themeMode)
 
+  const changeMode = () => {
+    dispatch(changeThemeModeAC({ themeMode: themeMode === "light" ? "dark" : "light" }))
+  }
+  const clickHandler = () => {
+    logout()
+      .then((res) => {
+        if (res.data?.resultCode === ResultCode.Success) {
+          dispatch(setIsLoggedInAC({ isLoggedIn: false }))
+          localStorage.removeItem(AUTH_TOKEN)
+          // dispatch(baseApi.util.resetApiState())
+        }
+      })
+      .then(() => {
+        dispatch(baseApi.util.invalidateTags(["Todolist", "Task"]))
+      })
+  }
+
+  return (
+    <AppBar position="static" sx={{ mb: "30px" }}>
+      <Toolbar>
+        <Container maxWidth={"lg"} sx={containerSx}>
+          <IconButton color="inherit">
+            <MenuIcon />
+          </IconButton>
+          <div>
+            {isLoggedIn && <NavButton onClick={clickHandler}>Log out</NavButton>}
+            <NavButton background={theme.palette.primary.dark}>Faq</NavButton>
+            <Switch color={"default"} onChange={changeMode} />
+          </div>
+        </Container>
+      </Toolbar>
+      {status === "loading" && <LinearProgress />}
+    </AppBar>
+  )
+}
